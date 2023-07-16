475f0632de0557f5bda828e118afcff052e11662
aditya <aditya28jha@appinventiv.com>
2023-07-16 21:31:50 +0530

    fixes

--- Utils/index.ts
+++ Utils/index.ts
@@ -1,20 +1,13 @@
-import api from "./api";
+
 import showAlert from "./alert";
 import endPoints from "./endPoints";
-import constants from "./constants";
-// import Validators from "./validators";
-import ActionName from "./actionName";
 import Pathname from "./pathname";
 import ConstFunction from "./constFunctions"
-// import CommonFunctions from "./commonFunctions";
 
 const Utils = {
-  api,
   showAlert,
   endPoints,
-  ActionName,
   Pathname,
-  constants,
   ConstFunction
     // Validators: Validators,
     // CommonFunctions: CommonFunctions,
