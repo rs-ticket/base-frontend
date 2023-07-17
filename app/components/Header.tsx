44815724c7dafe4dc9895094b92918b1661061c4
aditya <aditya28jha@appinventiv.com>
2023-07-17 23:33:32 +0530

    header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,24 +2,42 @@ import React from "react";
 import styles from '../../styles/Header.module.css'
 import { adminNavigation } from "@/Utils/constData";
 import Link from "next/link";
+import { AppBar, Box, Button, IconButton, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography } from "@mui/material";
 
 function Header() {
   return (
       <div className={styles.headerContainer}>
-<nav>
-  <ul>
-    {
+        <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          
+          {
       adminNavigation.map((link: any, index: any) => {
         return(
-          <li key={index}>
+          
           <Link href={link.path}>{link.name}</Link>
-        </li>
+       
+        
         )
         })
     }
-   
-  </ul>
-</nav>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+
     </div>
   );
 }
